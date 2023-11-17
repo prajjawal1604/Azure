467441b40f5cdc448c0f206cff93b899377fa61a
Prajjawal Pandit <panditprajjawal@gmail.com>
2023-11-17 11:33:05 +0530

    Add missing .terraform.* files to .gitignore

--- question01/loadBalancer.tf
+++ question01/loadBalancer.tf
@@ -12,6 +12,7 @@ resource "azurerm_lb" "lb01" {
 resource "azurerm_lb_backend_address_pool" "bpepool" {
   loadbalancer_id = azurerm_lb.lb01.id
   name            = "BackEndAddressPool"
+  
 }
 
 resource "azurerm_lb_nat_pool" "lbnatpool" {
