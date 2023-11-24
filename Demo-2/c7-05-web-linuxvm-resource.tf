0cef2471aa20cfd5a98ee4527ba2ab565a3b53e3
Prajjawal Pandit <panditprajjawal@gmail.com>
2023-11-24 16:38:47 +0530

    Demo-2 Script Updates

--- Demo-2/c7-05-web-linuxvm-resource.tf
+++ Demo-2/c7-05-web-linuxvm-resource.tf
@@ -1,21 +1,21 @@
 # Locals Block for custom data
-locals {
-  webvm_custom_data = <<CUSTOM_DATA
-    #!/bin/sh
-    #sudo yum update -y
-    sudo yum install -y httpd
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    sudo systemctl stop firewalld
-    sudo systemctl disable firewalld
-    sudo chmod -R 777 /var/www/html
-    sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html
-    sudo mkdir /var/www/html/app1
-    sudo echo "Hello World from $(hostname -f)" > /var/www/html/app1/index.html
-    sudo echo "WebVM App1 - AppStatus Page" > /var/www/html/app1/AppStatus.html
-    sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(255, 255, 0);"> <h1>WebVM App1 - AppStatus Page</h1> <p>App1 is running on WebVM App1</p> </body> </html>' > /var/www/html/app1/AppStatus.html
-    CUSTOM_DATA
-}
+# locals {
+#   webvm_custom_data = <<CUSTOM_DATA
+#     #!/bin/sh
+#     #sudo yum update -y
+#     sudo yum install -y httpd
+#     sudo systemctl enable httpd
+#     sudo systemctl start httpd
+#     sudo systemctl stop firewalld
+#     sudo systemctl disable firewalld
+#     sudo chmod -R 777 /var/www/html
+#     sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+#     sudo mkdir /var/www/html/app1
+#     sudo echo "Hello World from $(hostname -f)" > /var/www/html/app1/index.html
+#     sudo echo "WebVM App1 - AppStatus Page" > /var/www/html/app1/AppStatus.html
+#     sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(255, 255, 0);"> <h1>WebVM App1 - AppStatus Page</h1> <p>App1 is running on WebVM App1</p> </body> </html>' > /var/www/html/app1/AppStatus.html
+#     CUSTOM_DATA
+# }
 
 # Resource: Azure Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "web_linuxvm" {
@@ -40,6 +40,6 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
     sku       = "83-gen2"
     version   = "latest"
   }
-  # custom_data = filebase64("${path.module}/custom-data/webvm-custom-data.sh")
-  custom_data = base64encode(local.webvm_custom_data)
+  custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
+#   custom_data = base64encode(local.webvm_custom_data)
 }
