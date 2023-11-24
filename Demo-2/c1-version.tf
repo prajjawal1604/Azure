21741f94ba688908b777b68c8dbc26f3a200a0ef
Prajjawal Pandit <panditprajjawal@gmail.com>
2023-11-24 07:52:34 +0530

    Demo-2

---
+++ Demo-2/c1-version.tf
@@ -0,0 +1,22 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  #   required_version = "~> 1.0.0"  this can be used at production so that it only upgrades to minor version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
+}
+
+
